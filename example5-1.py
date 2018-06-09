f3fbc6f71d87a8226f3bf8fdf26712edda6faf59
yarikmik <yarik.mik@mail.ru>
2018-06-09 19:27:34 +0700

    2 step
    Попытка преобразовать маску

--- example5-1.py
+++ example5-1.py
@@ -16,6 +16,32 @@ Mask:
 {0:08b} {1:08b} {2:08b} {3:08b}
 '''
 
+# создание полной маски
+mask_full= "255.255.255.255"
+mask_full=mask_full.strip(',').split('.')
+
+'''
+In [11]: mask_full
+Out[11]: ['255', '255', '255', '255']
+'''
+# преобразование маски в бинарный вид
+mask_bin_str= '{:b}{:b}{:b}{:b}'.format(int(mask_full[0]),int(mask_full[1]),int(mask_full[2]),int(mask_full[3]))
+
+'''
+In [13]: mask_bin_str
+Out[13]: '11111111111111111111111111111111'
+
+In [17]: mask_bin_str[mask-1:]
+Out[17]: '111111111'
+'''
+
+# преборазование маски
+mask_bin_0=mask_bin_str.replace(mask_bin_str[mask:],'0'*(32-mask))
+mask_bin_0
+# Out[19]: '00000000000000000000000011111'
+
+
+
 
 
 #print('\n' )
