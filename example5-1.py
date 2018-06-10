1bf906ccc46ec68cce90782d643a42ef949a6863
yarikmik <yarik.mik@mail.ru>
2018-06-10 17:45:35 +0700

    5.1 ready

--- example5-1.py
+++ example5-1.py
@@ -1,6 +1,6 @@
 network = input('Enter network number:')
 mask = input('Enter mask number: /')
-
+mask=int(mask)
 network=network.strip(',').split('.')
 
 
@@ -12,6 +12,7 @@ Network:
 
 mask_template = '''
 Mask:
+/{mask}
 {0:<8} {1:<8} {2:<8} {3:<8}
 {0:08b} {1:08b} {2:08b} {3:08b}
 '''
@@ -31,26 +32,26 @@ mask_bin_str= '{:b}{:b}{:b}{:b}'.format(int(mask_full[0]),int(mask_full[1]),int(
 In [13]: mask_bin_str
 Out[13]: '11111111111111111111111111111111'
 
-In [17]: mask_bin_str[mask-1:]
-Out[17]: '111111111'
 '''
 
 # преборазование маски
-mask_bin_0=mask_bin_str.replace(mask_bin_str[mask:],'0'*(32-mask))
-mask_bin_0
-# Out[19]: '00000000000000000000000011111'
+mask_bin_0=mask_bin_str[:mask]+'0'*(32-mask)
+#In [9]: msk_bin_0
+#Out[9]: '11111111111111111111111100000000'
 
-
+# преобразование адреса в бинарный вид
+network=
 
 
 
 #print('\n' )
 print('\n' )
-print('\n' + '-'* 30)
+print('\n' + '-'* 35)
 #print(network)
 
 print(network_template.format(int(network[0]),int(network[1]),int(network[2]),int(network[3])))
 
+print(mask_template.format(int(mask_bin_0[0:8],2),int(mask_bin_0[8:16],2),int(mask_bin_0[16:24],2),int(mask_bin_0[24:32],2), mask=mask))
 
 
 
