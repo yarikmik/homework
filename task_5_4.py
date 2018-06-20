c2a6c1c628518dac2c0f3abc0b2ec244a2d7ea65
yarikmik <yarik.mik@mail.ru>
2018-06-20 19:40:47 +0700

    5.4 begin

---
+++ task_5_4.py
@@ -0,0 +1,8 @@
+
+num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
+word_list = ['python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl']
+
+num=tuple(num_list)
+word=tuple(word_list)
+name=input('Введите элемен, что бы узнать индекс последнего {}:'.format(num))
+name=input('Введите элемен, что бы узнать индекс последнего {}:'.format(word))
