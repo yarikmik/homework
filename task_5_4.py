0069b057f869f0b429cde174ac2dfd73b58b90b4
yarikmik <yarik.mik@mail.ru>
2018-06-23 18:17:46 +0700

    5.4 begin

--- task_5_4.py
+++ task_5_4.py
@@ -2,7 +2,20 @@
 num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
 word_list = ['python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl']
 
-num=tuple(num_list)
+num_str=tuple(num_list)
 word=tuple(word_list)
-name=input('Введите элемен, что бы узнать индекс последнего {}:'.format(num))
+
+num=input('Введите элемен, что бы узнать индекс последнего {}:'.format(num_str))
+
+
+
+num_list.index(int(num)) #проверка  индекса элемента
+
+
+
+
+
+
+
+
 name=input('Введите элемен, что бы узнать индекс последнего {}:'.format(word))
