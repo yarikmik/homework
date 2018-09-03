f9697aff205ec3ad5397d0fbb02f2771cce9d95c
yarikmik <yarik.mik@mail.ru>
2018-09-03 07:02:49 +0700

    7.2b in progress

--- task_7_2b.py
+++ task_7_2b.py
@@ -22,30 +22,49 @@
 
 ignore = ['duplex', 'alias', 'Current configuration']
 '''
-sw_conf=open('config_sw1.txt', 'r')
-sw_conf=sw_conf.read().strip().split('\n')
+sw=open('config_sw1.txt', 'r')
+sw_conf=sw.read().strip().split('\n')
+sw.close()
 ignore = ['duplex', 'alias', 'Current configuration']
 sw_print=[]
 
 
 
 for s in sw_conf:
-	s=s.strip('! ') # убираем символы пробела и !
+	s=s.strip(' ') # убираем символы пробела и !
 	if s=='':		# Пропускаем пустые строки
 		continue
 	else:
 		sw_print.append(s)
 		#print(s)
 
+'''
 for s in sw_print:
 	#for i in ignore:
 		if ignore[0] in s:
-			#print('найдено совпадение')
 			continue
 		elif ignore[1] in s:
 			continue
+		elif ignore[2] in s:
+			continue
 		else:
 			print(s)
+'''
 		
-		
+for s in sw_print:
+	for i in ignore:
+		if i in s:				#проверяем на наличие в строке элемента из фильтра
+			sw_print.remove(s)	#удаляем элемент из списка
+			print(s + ' >>>remove')			
+		else:
+			print (s + ' >>> NOT remove')	
+			
+'''
+f=open('config_sw1_cleared.txt', 'w')
+f.writelines(sw_print)
+f.close	
+'''
+print('\n' + '-'*35)
+for s in sw_print:
+	print(s)
 input()
