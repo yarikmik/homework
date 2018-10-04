cfc82895518c50805c72415b993f959e99821293
yarikmik <yarik.mik@mail.ru>
2018-10-04 14:50:55 +0700

    7_2b in progress

--- task_7_2b.py
+++ task_7_2b.py
@@ -28,43 +28,61 @@ sw.close()
 ignore = ['duplex', 'alias', 'Current configuration']
 sw_print=[]
 
-
-
+# При попытке удалять записи из уже существующего списка почему то некоторые строки не считаются, 
+# проще переписывать отсортированные строки в новый список
+"""
+# блок очищает от пустых строк и лишних пробелов
 for s in sw_conf:
-	s=s.strip(' ') # убираем символы пробела и !
+	s=s.strip(' ') # убираем символы пробела
 	if s=='':		# Пропускаем пустые строки
 		continue
 	else:
 		sw_print.append(s)
 		#print(s)
+""" 
+
+# блок сравнивающий множества из слов строки и списка фильтра(ignore), проблема в том, что третий элемент фильтра состоит из двух слов
+for s in sw_conf:
+		if not (set(ignore) & set(s.split())): # Проверка , что множество ignore пересекается со множеством s
+			#del sw_print[sw_print.index(s)]
+			sw_print.append(s)
+
+
+
+
+
+sw_print = [ line + '\n' for line in sw_print ]
+
 
 '''
 for s in sw_print:
-	#for i in ignore:
-		if ignore[0] in s:
-			continue
-		elif ignore[1] in s:
-			continue
-		elif ignore[2] in s:
-			continue
-		else:
-			print(s)
-'''
-		
-for s in sw_print:
+	print(s)
+print('\n' + '-'*35)
+
+for s in sw_conf:
 	for i in ignore:
 		if i in s:				#проверяем на наличие в строке элемента из фильтра
-			sw_print.remove(s)	#удаляем элемент из списка
-			print(s + ' >>>remove')			
-		else:
-			print (s + ' >>> NOT remove')	
+			sw_conf.remove(s)	#удаляем элемент из списка
+
+f=open('config_sw1_cleared.txt', 'w')
+f.writelines(sw_conf)
+f.close	
+
+print('\n' + '-'*35)
+for s in sw_conf:
+	print(s)
+
+input()
 			
 '''
+
+
 f=open('config_sw1_cleared.txt', 'w')
 f.writelines(sw_print)
 f.close	
-'''
+
 print('\n' + '-'*35)
 for s in sw_print:
 	print(s)
+
 input()
