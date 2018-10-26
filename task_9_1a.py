6dad4e7ab90c639a3afb4c8144b50c2a20754fdb
yarikmik <yarik.mik@mail.ru>
2018-10-26 16:05:54 +0700

    9_1a complite

---
+++ task_9_1a.py
@@ -0,0 +1,63 @@
+'''
+Функция ожидает, как аргумент, словарь access-портов, вида:
+
+{'FastEthernet0/12':10,
+ 'FastEthernet0/14':11,
+ 'FastEthernet0/16':17,
+ 'FastEthernet0/17':150}
+Функция должна возвращать список всех портов в режиме access с конфигурацией на основе шаблона access_template.
+
+В конце строк в списке не должно быть символа перевода строки.
+
+Пример итогового списка (перевод строки после каждого элемента сделан для удобства чтения):
+
+[
+'interface FastEthernet0/12',
+'switchport mode access',
+'switchport access vlan 10',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+'interface FastEthernet0/17',
+'switchport mode access',
+'switchport access vlan 150',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+...]
+'''
+
+
+access_dict = { 'FastEthernet0/12':10,
+				'FastEthernet0/14':11,
+				'FastEthernet0/16':17,
+				'FastEthernet0/17':150 }
+
+
+def generate_access_config(access, portsec=False):
+	"""генератор конфигурации access для портов"""
+	access_template = ['switchport mode access',
+					   'switchport access vlan',
+					   'switchport nonegotiate',
+					   'spanning-tree portfast',
+					   'spanning-tree bpduguard enable']
+	
+	port_security = ['switchport port-security maximum 2',
+					 'switchport port-security violation restrict',
+					 'switchport port-security']
+
+	access_port=[]
+	
+	for intf, vlan in access.items():
+		access_port.append('interface' + intf )
+		
+		for line in access_template:
+			if line.endswith('vlan'):
+				access_port.append(line + ' ' + str(vlan))
+			else:
+				access_port.append(line)
+		if portsec:
+				access_port.extend(port_security)
+	return access_port
+
+print('\n'.join(generate_access_config(access_dict, True)))
