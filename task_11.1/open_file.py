fe8fa473ede36f491959646c9ec5226ad5e9f9ba
yarikmik <yarik.mik@gmail.com>
2018-12-15 17:05:24 +0700

    11_1 complete

---
+++ task_11.1/open_file.py
@@ -0,0 +1,6 @@
+
+
+with open('sw1_sh_cdp_neighbors.txt', 'r') as file:
+	line = file.read()
+	
+#print(line) 
---
+++ task_11.1/task_11_1.py
@@ -0,0 +1,43 @@
+'''
+\Создать функцию parse_cdp_neighbors, которая обрабатывает вывод команды show cdp neighbors.
+
+Функция ожидает, как аргумент, вывод команды одной строкой (а не имя файла).
+
+Функция должна возвращать словарь, который описывает соединения между устройствами.
+
+Например, если как аргумент был передан такой вывод:
+
+R4>show cdp neighbors
+
+Device ID	 Local Intrfce	 Holdtme	 Capability		  Platform	  Port ID
+R5			 Fa 0/1			 122		   R S I		   2811		  Fa 0/1
+R6			 Fa 0/2			 143		   R S I		   2811		  Fa 0/0
+Функция должна вернуть такой словарь:
+
+{('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+ ('R4', 'Fa0/2'): ('R6', 'Fa0/0')}
+Интерфейсы могут быть записаны с пробелом Fa 0/0 или без Fa0/0.
+
+Проверить работу функции на содержимом файла sw1_sh_cdp_neighbors.txt
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+from open_file import line
+
+def parse_cdp_neighbors(cdp):
+	for s in cdp:
+		if s == '>':
+			self_name = cdp[0:cdp.index(s)] #мыводим собственное имя устройства
+	list=cdp.split('\n')
+	cdp_neighbors = {}
+	for s in list:
+		if s.startswith('R'):				#расспаковываем переменные по нужным параметра соседа
+			neighbors=s.split()
+			n_router, int, local_int, port, port_id = neighbors[0],neighbors[1], neighbors[2], neighbors[-2], neighbors[-1]
+			key=[self_name, int+local_int] 
+			value=[n_router, port+port_id] #присваиваем нужные параметры переменным для словаря
+			cdp_neighbors[tuple(key)]=tuple(value)
+	return cdp_neighbors
+
+print(parse_cdp_neighbors(line))
