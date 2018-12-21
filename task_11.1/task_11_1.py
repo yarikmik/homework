eb015c4c307d30c1284d3feec4a5a4aaa8480905
yarikmik <yarik.mik@mail.ru>
2018-12-21 18:23:51 +0700

    11_1 ready

--- task_11.1/task_11_1.py
+++ task_11.1/task_11_1.py
@@ -26,14 +26,20 @@ R6			 Fa 0/2			 143		   R S I		   2811		  Fa 0/0
 from open_file import line
 
 def parse_cdp_neighbors(cdp):
+
+	'''функция возвращает словарь из пары ключ(имя собственного устройства, интерфейс подключения) 
+	и значение (имя устройства соседа, интерфейс подключения соседа)'''
+
 	for s in cdp:
 		if s == '>':
 			self_name = cdp[0:cdp.index(s)] #мыводим собственное имя устройства
 	list=cdp.split('\n')
 	cdp_neighbors = {}
 	for s in list:
-		if s.startswith('R'):				#расспаковываем переменные по нужным параметра соседа
-			neighbors=s.split()
+		if s.startswith('R' or 'T' or 'B' or 'S' 
+		or 'H' or 'I' or 'r' or 'P') and s[1].isdigit:	# проверяем строку на условие
+			neighbors=s.split()	
+			#расспаковываем переменные по нужным параметрам соседа:
 			n_router, int, local_int, port, port_id = neighbors[0],neighbors[1], neighbors[2], neighbors[-2], neighbors[-1]
 			key=[self_name, int+local_int] 
 			value=[n_router, port+port_id] #присваиваем нужные параметры переменным для словаря
