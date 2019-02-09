2652b3111c31e8f9edaee4f0f8a266d7de7970d5
yarikmik <yarik.mik@gmail.com>
2019-02-09 16:12:03 +0700

    12_1 in progress

--- task_12_1/task_12_1.py
+++ task_12_1/task_12_1.py
@@ -14,18 +14,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
-
+import platform
+import subprocess
 
 with open('ip_list.txt', 'r') as file:
-	ip_list=[]
-	for line in file:
-		ip_list.append(line.split('\n'))
-	
-	
-	
-
+	ip_list=file.read().rstrip().split('\n') #записываем адреса из файла в список
 
 print (ip_list)
+
+
+
+def ping(host):
+
+	'''Функция проверяет адрес на доступность (ping), если доступно возвращает True
+	проверяет платформу'''
+	param = '-n' if platform.system()=='Windows' else '-c' #проверка платформы для определения ключа
+	shell_needed = True if platform.system()=='Windows' else False #проверка плотформы для определения необходимости в shell
+	ping_command = ['ping', param, '1', host] # запись команды пинга
+	
+	ping_output = subprocess.run(ping_command,shell=shell_needed,stdout=subprocess.PIPE)
+	
+	success = ping_output.returncode
+	return True if success == 0 else False
+
+print(ping('127.0.0.1'))
+
 
 def check_ip_addresses(ip_list):
 	'''Функция ожидает как аргумент список IP-адресов.
