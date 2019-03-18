517aaec6bf45e2a18f599770c9888ff0d26dc772
yarikmik <yarik.mik@mail.ru>
2019-03-18 17:03:59 +0700

    task 12_3 complete

--- task_12_1/task_12_3.py
+++ task_12_1/task_12_3.py
@@ -24,22 +24,38 @@ Reachable    Unreachable
 import ipaddress
 import platform
 import subprocess
+from tabulate import tabulate
 from task_12_1 import ping, check_ip_addresses
 from task_12_2 import check_ip_availability
+from itertools import zip_longest # позволяет зиповать списки не по кратчайшему, а по длиннейшему из зипуемых объектов
+
 
 with open('adress_list.txt', 'r') as file:
 	adress_list=file.read().rstrip().split('\n') #записываем адреса из файла в список
 
 
-
-	
-def ip_table(available, not_available):
-	
-
-
-
-
-
 ip_list=check_ip_availability(adress_list)
-
 available, not_available = check_ip_addresses(ip_list)
+
+def ip_table(available, not_available):
+	'''Функция ожидает как аргументы два списка:
+		* список доступных IP-адресов
+		* список недоступных IP-адресов
+
+		Результат работы функции - вывод на стандартный поток вывода таблицы вида:
+
+		Reachable    Unreachable
+		-----------  -------------
+		10.1.1.1     10.1.1.7
+		10.1.1.2     10.1.1.8
+					 10.1.1.9
+	'''
+	zip_list=list(zip_longest(available, not_available, fillvalue='')) # сливаем два списка в один список по парам- доступный-не доступный-не
+	return zip_list
+
+zip_list=ip_table(available, not_available)
+	
+columns=['Доступные IP:', 'Недоступные IP:']
+print(tabulate(zip_list, headers=columns))
+
+
